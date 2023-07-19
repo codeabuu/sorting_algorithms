df6a284efc7031497fe75e21fb88986945a5475c
codeabuu <abdulngui718@gmail.com>
2023-07-19 05:53:04 -0700

    sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,18 +9,13 @@
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
 	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
-	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
+    n2->next != NULL ? n2->next->prev = *n1 : NULL;
+    n2->prev = (*n1)->prev;
+    n2->next = *n1;
+    (*n1)->prev != NULL ? (*n1)->prev->next = n2 : (*h = n2);
+    (*n1)->prev = n2;
+    *n1 = n2->prev;
 }
-
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers
  *                       using the insertion sort algorithm.
@@ -39,7 +34,7 @@ void insertion_sort_list(listint_t **list)
 	{
 		tmp = iter->next;
 		insert = iter->prev;
-		while (insert != NULL && iter->n < insert->n)
+		for (; insert != NULL && iter->n < insert->n; insert = iter->prev)
 		{
 			swap_nodes(list, &insert, iter);
 			print_list((const listint_t *)*list);
