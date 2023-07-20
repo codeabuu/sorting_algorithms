1ae36cfc687cc3ddd5f758a9dc9e2671b052efcc
lawrence1986 <madusquare@gmail.com>
2023-07-20 11:26:28 -0700

    Solved betty related issues

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,28 +1,28 @@
 #include "sort.h"
 
 /**
- * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
- * @h: A pointer to the head of the doubly-linked list.
- * @n1: A pointer to the first node to swap.
- * @n2: The second node to swap.
- */
+* swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+* @h: A pointer to the head of the doubly-linked list.
+* @n1: A pointer to the first node to swap.
+* @n2: The second node to swap.
+*/
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
 	(*n1)->next = n2->next;
-    n2->next != NULL ? n2->next->prev = *n1 : NULL;
-    n2->prev = (*n1)->prev;
-    n2->next = *n1;
-    (*n1)->prev != NULL ? (*n1)->prev->next = n2 : (*h = n2);
-    (*n1)->prev = n2;
-    *n1 = n2->prev;
+n2->next != NULL ? n2->next->prev = *n1 : NULL;
+n2->prev = (*n1)->prev;
+n2->next = *n1;
+(*n1)->prev != NULL ? (*n1)->prev->next = n2 : (*h = n2);
+(*n1)->prev = n2;
+*n1 = n2->prev;
 }
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers
- *                       using the insertion sort algorithm.
- * @list: A pointer to the head of a doubly-linked list of integers.
- *
- * Description: Prints the list after each swap.
- */
+* insertion_sort_list - Sorts a doubly linked list of integers
+*                       using the insertion sort algorithm.
+* @list: A pointer to the head of a doubly-linked list of integers.
+*
+* Description: Prints the list after each swap.
+*/
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *iter, *insert, *tmp;
