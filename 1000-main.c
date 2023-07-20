d6bf7efdc9551e91da4378040bcfc7adcf93a45a
lawrence1986 <madusquare@gmail.com>
2023-07-20 10:36:20 -0700

    Solved betty related issues on 1000-main.c

--- 1000-main.c
+++ 1000-main.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include "deck.h"
 
+/**
+* print_deck - Prints the contents of a deck of cards.
+* @deck: A pointer to the head of the linked list representing the deck.
+*/
+
 void print_deck(const deck_node_t *deck)
 {
 size_t i;
@@ -19,6 +24,12 @@ i = (i + 1) % 13;
 deck = deck->next;
 }
 }
+
+/**
+* init_deck - Initializes a deck of cards.
+* @cards: An array of card_t structs representing the deck's cards.
+* Return: A pointer to the head of the linked list representing the deck.
+*/
 
 deck_node_t *init_deck(const card_t cards[52])
 {
@@ -43,11 +54,28 @@ deck = node;
 return (deck);
 }
 
+/**
+* main - The main function that executes the program.
+* Return: 0 upon successful execution.
+*/
+
 int main(void)
 {
 card_t cards[52] = {
-{"Jack", CLUB}, {"4", HEART}, {"3", HEART}, {"3", DIAMOND}, {"Queen", HEART}, {"5", HEART}, {"5", SPADE}, {"10", HEART}, {"6", HEART}, {"5", DIAMOND}, {"6", SPADE}, {"9", HEART}, {"7", DIAMOND}, {"Jack", SPADE}, {"Ace", DIAMOND}, {"9", CLUB}, {"Jack", DIAMOND}, {"7", SPADE}, {"King", DIAMOND}, {"10", CLUB}, {"King", SPADE}, {"8", CLUB}, {"9", SPADE}, {"6", CLUB}, {"Ace", CLUB}, {"3", SPADE}, {"8", SPADE}, {"9", DIAMOND}, {"2", HEART}, {"4", DIAMOND}, {"6", DIAMOND}, {"3", CLUB}, {"Queen", CLUB}, {"10", SPADE}, {"8", DIAMOND}, {"8", HEART}, {"Ace", SPADE}, {"Jack", HEART}, {"2", CLUB}, {"4", SPADE}, {"2", SPADE}, {"2", DIAMOND}, {"King", CLUB}, {"Queen", SPADE}, {"Queen", DIAMOND}, {"7", CLUB}, {"7", HEART}, {"5", CLUB}, {"10", DIAMOND}, {"4", CLUB}, {"King", HEART}, {"Ace", HEART},
+{"Jack", CLUB}, {"4", HEART}, {"3", HEART}, {"3", DIAMOND},
+{"Queen", HEART}, {"5", HEART}, {"5", SPADE}, {"10", HEART},
+{"6", HEART}, {"5", DIAMOND}, {"6", SPADE}, {"9", HEART},
+{"7", DIAMOND}, {"Jack", SPADE}, {"Ace", DIAMOND}, {"9", CLUB},
+{"Jack", DIAMOND}, {"7", SPADE}, {"King", DIAMOND}, {"10", CLUB},
+{"King", SPADE}, {"8", CLUB}, {"9", SPADE}, {"6", CLUB}, {"Ace", CLUB},
+{"3", SPADE}, {"8", SPADE}, {"9", DIAMOND}, {"2", HEART}, {"4", DIAMOND},
+{"6", DIAMOND}, {"3", CLUB}, {"Queen", CLUB}, {"10", SPADE}, {"8", DIAMOND},
+{"8", HEART}, {"Ace", SPADE}, {"Jack", HEART}, {"2", CLUB}, {"4", SPADE},
+{"2", SPADE}, {"2", DIAMOND}, {"King", CLUB}, {"Queen", SPADE},
+{"Queen", DIAMOND}, {"7", CLUB}, {"7", HEART}, {"5", CLUB}, {"10", DIAMOND},
+{"4", CLUB}, {"King", HEART}, {"Ace", HEART},
 };
+
 deck_node_t *deck;
 
 deck = init_deck(cards);
