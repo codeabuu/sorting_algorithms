5f989290b2677eaa43a80121b303a3899bf18d37
codeabuu <abdulngui718@gmail.com>
2023-07-22 03:01:37 -0700

    sort deck

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -17,10 +17,8 @@ void sort_deck(deck_node_t **deck);
  */
 int _strcmp(const char *s1, const char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2)
+	for (; *s1 && *s2 && *s1 == *s2; s1++, s2++)
 	{
-		s1++;
-		s2++;
 	}
 
 	if (*s1 != *s2)
